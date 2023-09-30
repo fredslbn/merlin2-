d93c205300a1a35f5633fe1f745d0709bbd72da3
fredslbn <fh.silaban@gmail.com>
2023-09-30 10:10:51 +0700

    add local

--- .circleci/merlin.sh
+++ .circleci/merlin.sh
@@ -11,7 +11,7 @@ KERNEL_DIR="$(pwd)"
 ##----------------------------------------------------------##
 # Device Name and Model
 MODEL=Xiaomi
-DEVICE=lancelot
+DEVICE=merlin
 
 # Kernel Version Code
 #VERSION=
@@ -32,16 +32,16 @@ IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 VERBOSE=0
 
 # Kernel Version
-KERVER=$(make kernelversion)
+#KERVER=$(make kernelversion)
 
-COMMIT_HEAD=$(git log --oneline -1)
+#COMMIT_HEAD=$(git log --oneline -1)
 
 # Date and Time
 DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M")
 TANGGAL=$(date +"%F%S")
 
 # Specify Final Zip Name
-ZIPNAME=SUPER.KERNEL
+ZIPNAME="SUPER.KERNEL-MERLIN-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
 FINAL_ZIP=${ZIPNAME}-${DEVICE}-${DATE}.zip
 FINAL_ZIP_ALIAS=Karenulmerlita-${DATE}.zip
 
@@ -121,7 +121,7 @@ function cloneTC() {
 	fi
 	
     # Clone AnyKernel
-    git clone --depth=1 https://github.com/missgoin/AnyKernel3.git
+    #git clone --depth=1 https://github.com/missgoin/AnyKernel3.git
 
 	}
 
@@ -221,6 +221,7 @@ START=$(date +"%s")
 	       #READELF=llvm-readelf \
 	       #OBJSIZE=llvm-size \
 	       V=$VERBOSE 2>&1 | tee error.log
+	       
 	elif [ -d ${KERNEL_DIR}/cosmic ];
 	   then
 	       make -j$(nproc --all) O=out \
@@ -301,12 +302,12 @@ function zipping() {
 	
 	# Zipping and Push Kernel
 	cd AnyKernel3 || exit 1
-        zip -r9 ${FINAL_ZIP} *
-        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-        echo "Zip: $FINAL_ZIP"
-        #curl -T $FINAL_ZIP_ALIAS temp.sh; echo
-        #curl -T $FINAL_ZIP_ALIAS https://oshi.at; echo
-        curl --upload-file $FINAL_ZIP https://free.keep.sh; echo
+        zip -r9 ${ZIPNAME} *
+        MD5CHECK=$(md5sum "$ZIPNAME" | cut -d' ' -f1)
+        echo "Zip: $ZIPNAME"
+        #curl -T $ZIPNAME temp.sh; echo
+        #curl -T $ZIPNAME https://oshi.at; echo
+        curl --upload-file $ZIPNAME https://free.keep.sh
     cd ..
 }
     
